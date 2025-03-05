6cb70fc0533403b44958f0af8eccd8569d84abb9
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-03-05 11:28:32 -0300

    fix: use sudo for package installation in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Instalando dependências do sistema..."
-apt-get update && apt-get install -y libzbar0
+sudo apt-get update && sudo apt-get install -y libzbar0
 
 echo "Iniciando a aplicação..."
 uvicorn main:app --host 0.0.0.0 --port $PORT
